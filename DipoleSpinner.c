40d845c50573174af962a69b3667a2c5ee08a718
indigoYoshimaru <48859573+indigoYoshimaru@users.noreply.github.com>
2019-06-15 01:36:40 +0700

    rearrange, add comments

--- DipoleSpinner.c
+++ DipoleSpinner.c
@@ -10,8 +10,8 @@ Vector2 BallPosition1,BallPosition2;
 Vector2 Center; 
 double Angle, Radius, AngleSpeed=PI/180;
 int Score;
-char ScoreText[1024];
-bool gameOver=false;
+char ScoreText[10];
+bool gameOver;
 
 struct Obstacle{
     double size;
@@ -34,10 +34,7 @@ void BallSpin(void);
 
 int main(void)
 {
-    // Initialization (Note windowTitle is unused on Android)
-    //---------------------------------------------------------
     InitWindow(640, 480, "Dipole Spinner");
-    
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
     GameInit();
@@ -52,25 +49,22 @@ int main(void)
        }
        
        BallSpin();
-       BallPosition1.x=Center.x+Radius*cos(Angle);
-       BallPosition1.y=Center.y+Radius*sin(Angle);
        
-       BallPosition2.x=Center.x-Radius*cos(Angle);
-       BallPosition2.y=Center.y-Radius*sin(Angle);
        for (int i=0; i<currentPosition; i++)
        {
-            struct Obstacle* ob=&Obstacles[i];
+            struct Obstacle* ob=&Obstacles[i]; //set the pointer ob to the adress of Obstacles[i] 
             
-            if (ob->active)
+            if (ob->active)             // if the obstacle is active
             {
-                if (ob->position.x>480)
+                if (ob->position.x>480) // if its x >480 (320+(2r=40)+(R=120)
                 {
-                    Score++; 
-                    ob->active=false;
+                    Score++;            
+                    ob->active=false;   //set the active status to false
                 }
-                else if (ob->position.x>160)
+                else if (ob->position.x>160)    // if it's inside the big circle
                 {
-                    if ((ob->position.y<80&&ob->initialPosition.y>240)||(ob->position.y>400&&ob->initialPosition.y<=240))
+                    if ((ob->position.y<80&&ob->initialPosition.y>240)||    //obstacle moves up & exceed the min y
+                        (ob->position.y>400&&ob->initialPosition.y<=240))   //obstacle moves down & exceed the max y
                     {
                         Score++;
                         ob->active=false;
@@ -79,20 +73,17 @@ int main(void)
                 
             }
             
-            if (ob->position.x<0||ob->position.x>640||ob->position.y<0||ob->position.y>480)
+            if (ob->position.x<0||ob->position.x>640||ob->position.y<0||ob->position.y>480) //obstacle out of screen
             {
                 currentPosition--;
-                Obstacles[i]=Obstacles[currentPosition];
+                *ob=Obstacles[currentPosition]; //set the last obstacle of array to the empty position 
                 continue;
             }
             ObstacleMove(i);  
-       }
-            
+       }   
        DrawGame();
     }
-    
     CloseWindow();        
-   
     return 0;
 }
 
@@ -105,9 +96,56 @@ void GameInit(void)
     Score=0;
     Angle=0, Radius=120, AngleSpeed=PI/180;
     gameOver=false;
+}
+
+void BallSpin(void)
+{
+    if (IsKeyDown(KEY_SPACE)&&!gameOver)
+        Angle+=AngleSpeed*8;
     
-    sprintf(ScoreText, "%d", Score);
-  
+    Angle-=AngleSpeed*2;
+    
+    BallPosition1.x=Center.x+Radius*cos(Angle);
+    BallPosition1.y=Center.y+Radius*sin(Angle);
+   
+    BallPosition2.x=Center.x-Radius*cos(Angle);
+    BallPosition2.y=Center.y-Radius*sin(Angle);
+}
+
+void GenerateObstacle(void)
+{
+    struct Obstacle* ob=&Obstacles[currentPosition];
+    ob->position.x=0;
+    ob->position.y=rand()%480;          //obstacle moves vertically random from the left
+    ob->size=20+rand()%20;              //obstacles' size ranges randomly in range[20,40]
+    ob->initialPosition=ob->position;   
+    
+    ob->velocity.x=rand()%2+1;          //random velocity.x &y
+    ob->velocity.y=rand()%2+1;
+    
+    ob->active=true;
+    currentPosition++;
+}
+
+void ObstacleMove(int i)
+{
+    struct Obstacle* ob=&Obstacles[i];
+    if (CheckCollisionCircles(BallPosition1,20,ob->position,ob->size) 
+            ||CheckCollisionCircles(BallPosition2,20,ob->position,ob->size))
+    {
+        gameOver=true;      //collide --> game over
+        return;
+    }
+        
+    if (ob->initialPosition.y>240)      //the obstacle move up if from the bottom of screen
+    {
+        ob->position.x+=3*ob->velocity.x;
+        ob->position.y-=ob->velocity.y;
+        return;
+    }
+    
+    ob->position.x+=3*ob->velocity.x;   
+    ob->position.y+=ob->velocity.y; 
 }
 
 void DrawGame(void)
@@ -115,28 +153,30 @@ void DrawGame(void)
     BeginDrawing();
     
     ClearBackground(RAYWHITE);
-    if (!gameOver)
+    if (!gameOver)      
     {
         DrawCircleV(BallPosition1, 20, MAROON);
         DrawCircleV(BallPosition2, 20, DARKBLUE);
         
         for (int i=0; i<currentPosition; i++)
-        {
             DrawCircleV(Obstacles[i].position,Obstacles[i].size ,BLACK);
-        }
-        sprintf(ScoreText, "%d", Score);
+        
+        sprintf(ScoreText, "%d", Score); //score-> string and store in ScoreText
         DrawText(ScoreText,560, 10, 40, GRAY);
         
     }
     
     else    
     {
-        DrawText("GAME OVER", GetScreenWidth()/2 - MeasureText("GAME OVER", 60)/2, 100, 60, BLACK);
-        char scoreString[30]="YOUR SCORE IS: ";
+        DrawRectangle(20,40,600,400,SKYBLUE);
+        Rectangle rec={20,40,600,400};
+        DrawRectangleLinesEx(rec,20,BLUE);
+        DrawText("GAME OVER", GetScreenWidth()/2 - MeasureText("GAME OVER", 60)/2, 100, 60, BLACK); //text,x,y,fontsize,color
+        char scoreString[50]="YOUR SCORE IS: ";
         strcat(scoreString,ScoreText);
-        DrawText(scoreString, GetScreenWidth()/2 - MeasureText(scoreString, 40)/2, GetScreenHeight()/2 -40, 40, GRAY);
-        DrawText("Press [ENTER] to replay", GetScreenWidth()/2 - MeasureText("Press [ENTER] to replay", 40)/2, GetScreenHeight()/2 , 40, GRAY);
-
+        DrawText(scoreString, GetScreenWidth()/2 - MeasureText(scoreString, 40)/2, GetScreenHeight()/2 -40, 40, DARKGRAY);
+        DrawText("Press [ENTER] to replay", GetScreenWidth()/2 - MeasureText("Press [ENTER] to replay", 40)/2, 
+            GetScreenHeight()/2 , 40, DARKGRAY);
         
         if (IsKeyDown(KEY_ENTER))
         {
@@ -148,49 +188,7 @@ void DrawGame(void)
     EndDrawing();
 }
 
-void GenerateObstacle(void)
-{
-    Obstacles[currentPosition].position.x=0;
-    Obstacles[currentPosition].position.y=rand()%480;
-    Obstacles[currentPosition].size=20;
-    Obstacles[currentPosition].initialPosition=Obstacles[currentPosition].position;
-    
-    Obstacles[currentPosition].velocity.x=rand()%2+1;
-    Obstacles[currentPosition].velocity.y=rand()%2+1;
-    
-    Obstacles[currentPosition].active=true;
-    currentPosition++;
-    
-}
 
-void ObstacleMove(int i)
-{
-    if (CheckCollisionCircles(BallPosition1,20,Obstacles[i].position,20)||CheckCollisionCircles(BallPosition2,20,Obstacles[i].position,20))
-    {
-        gameOver=true;
-        return;
-    }
-        
-    if (Obstacles[i].initialPosition.y>240)
-    {
-        Obstacles[i].position.x+=3*Obstacles[i].velocity.x;
-        Obstacles[i].position.y-=Obstacles[i].velocity.y;
-        return;
-    }
-    
-        Obstacles[i].position.x+=3*Obstacles[i].velocity.x;
-        Obstacles[i].position.y+=Obstacles[i].velocity.y; 
-     
-    
-}
 
-void BallSpin(void)
-{
-    if (IsKeyDown(KEY_SPACE)&&!gameOver)
-    {
-        Angle+=AngleSpeed*8;
-    }
-    Angle-=AngleSpeed*2;
-}
 
 
